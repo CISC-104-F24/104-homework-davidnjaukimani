b00575d8de4a13ea56a439b0f2dc8fa4523ffe12
davidnjaukimani <raynjau@udel.edu>
2024-09-07 07:24:02 -0400

    Completed Homework0

--- Homework Project/Assets/Scripts/HelloWorld.cs
+++ Homework Project/Assets/Scripts/HelloWorld.cs
@@ -8,6 +8,14 @@ public class HelloWorld : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello, world!");
+        Debug.LogWarning("I should fix my sleep schedule.");
+        Debug.LogError("David flew too close to the sun");
+        //Author: David Njau Kimani
+        /*
+         * What do you think the most challenging aspect of this class is (or the one that people tend to struggle with/should pay the most attention to)
+         * A visually simple roguelike with procedurally generated items, characters and levels
+         * I hope to learn more about procedural generation
+         */
     }
 
     // Update is called once per frame
