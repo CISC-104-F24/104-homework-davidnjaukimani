5a3b0e495016a49fc1765d717bd310e684fc1486
davidnjaukimani <raynjau@udel.edu>
2024-12-16 23:53:06 -0500

    Homework 4 done
    HOMEWORK 4 DONE

---
+++ Homework Project/Assets/Scripts/Homework 4/BEANSINCREASE.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;  // <-- Add this line to use TextMeshPro
+
+public class Increasebeans : MonoBehaviour
+{
+    TMP_Text textField;  // Reference to the TextMeshPro component
+    int number = 0;  // Initialize the number
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the GameObject by name and get the TextMeshPro component
+        textField = GameObject.Find("beansCounter").GetComponent<TMP_Text>();
+
+        // Check if the textField was successfully found
+        if (textField == null)
+        {
+            Debug.LogError("TextMeshPro component not found on 'hpCounter' GameObject!");
+        }
+    }
+
+    public void ChangeText()
+    {
+        number += 10;
+        textField.text = number.ToString();
+    }
+}
